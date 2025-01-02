ed8d00e5ef9d353ac4d3a9c30f82804dab945700
Sumaya05Ali <123470887+Sumaya05Ali@users.noreply.github.com>
2025-01-02 15:30:24 +0600

    Update admin.py

--- trip_scraper/property_data/admin.py
+++ trip_scraper/property_data/admin.py
@@ -1,7 +1,7 @@
 # admin.py
 
 from django.contrib import admin
-from .models import Hotel, PropertySummary
+from .models import Hotel, PropertySummary, PropertyReview
 
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('title', 'rating', 'location', 'price', 'city_id')  # Add other fields as needed
@@ -37,5 +37,36 @@ class PropertySummaryAdmin(admin.ModelAdmin):
     # Add readonly fields
     readonly_fields = ('property_id',)    
 
+class PropertyReviewAdmin(admin.ModelAdmin):
+    list_display = ('property_id', 'rating', 'truncated_review', 'hotel_title')
+    search_fields = ('property_id', 'review')
+    list_filter = ('rating',)
+    readonly_fields = ('property_id',)
+    
+    def truncated_review(self, obj):
+        """Display a truncated version of the review in the list view"""
+        return obj.review[:150] + '...' if len(obj.review) > 150 else obj.review
+    truncated_review.short_description = 'Review'
+    
+    def hotel_title(self, obj):
+        """Display the related hotel title"""
+        try:
+            hotel = Hotel.objects.get(id=obj.property_id)
+            return hotel.title
+        except Hotel.DoesNotExist:
+            return 'N/A'
+    hotel_title.short_description = 'Hotel Title'
+    
+    fieldsets = (
+        ('Property Information', {
+            'fields': ('property_id', 'rating')
+        }),
+        ('Review Content', {
+            'fields': ('review',)
+        }),
+    )
+
+
 admin.site.register(Hotel, HotelAdmin)
 admin.site.register(PropertySummary, PropertySummaryAdmin)
+admin.site.register(PropertyReview, PropertyReviewAdmin)
